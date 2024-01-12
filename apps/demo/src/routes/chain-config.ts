2ff67d4df1c4d7cc5ff8914a7fd4bf7a5d3835c6
dfokin <dmitry.fokin@macrodemic.com>
2024-01-12 10:06:27 +0200

    prettier

--- apps/demo/src/routes/chain-config.ts
+++ apps/demo/src/routes/chain-config.ts
@@ -7,11 +7,11 @@ export const mumbai = {
     name: 'MATIC',
     symbol: 'MATIC',
     decimals: 18,
-  }
+  },
 }
 
 export const spicy = {
-  chainId:'0x15b32',
+  chainId: '0x15b32',
   rpcUrl: 'https://chiliz-spicy.publicnode.com',
   displayName: 'Spicy',
   blockExplorer: 'https://spicy-explorer.chiliz.com/',
@@ -19,5 +19,5 @@ export const spicy = {
     name: 'CHZ',
     symbol: 'CHZ',
     decimals: 18,
-  }
+  },
 }
