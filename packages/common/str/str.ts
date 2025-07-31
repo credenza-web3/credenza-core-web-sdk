5acc3fc5a61cccc23d630c7a9f5b030b58171ca3
mavvad <mavvad00@gmail.com>
2025-07-31 15:35:47 +0200

    fixed an issue with state being uriEncoded from the backend response while compairing to state from LS

--- packages/common/str/str.ts
+++ packages/common/str/str.ts
@@ -1,5 +1,5 @@
 export function generateCode(length: number = 64): string {
-  const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~'
+  const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
   const array = new Uint8Array(length)
   crypto.getRandomValues(array)
   return Array.from(array, (byte) => charset[byte % charset.length]).join('')
